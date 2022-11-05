acc219183240f4e050021d37575daf3bab551f05
devEMEL <dominichenry882@gmail.com>
2022-11-05 17:41:07 +0100

    Fund acount

--- scripts/asset_funciions.js
+++ scripts/asset_funciions.js
@@ -9,3 +9,35 @@ const { addr: managerAddr } = creator;
 const { addr: clawbackAddr } = creator;
 const { addr: reserveAddr } = creator;
 
+const { addr: newClawbackAddr } = algosdk.mnemonicToSecretKey(process.env.ACC1_MNEMONIC);
+
+
+const submitToNetwork = async (signedTxn) => {
+
+    // Send transaction
+    const tx = await algodClient.sendRawTransaction(signedTxn).do();
+    console.log(`Transaction: ${tx.txId}`);
+    
+    // Wait for transaction to be confirmed
+    confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4)
+
+    // Get the completed transaction
+    console.log(`Transaction: ${tx.txId} confirmed in round ${confirmedTxn["confirmed-round"]}`);
+    
+
+    return confirmedTxn;
+}
+
+const fundAccount = async (receiver, amount) => {
+
+    const  suggestedParams = await algodClient.getTransactionParams().do();
+    const txn = algosdk.makePaymentTxnWithSuggestedParams(
+        creator.addr,
+        receiver.addr,
+        amount,
+        undefined,
+        undefined,
+        suggestedParams
+    );
+
+}
