d8854b32bee83ed58890088778e4264f97a381a5
devEMEL <dominichenry882@gmail.com>
2022-11-05 13:02:58 +0100

    create asset function

---
+++ scripts/create_nft.js
@@ -0,0 +1,75 @@
+
+const algosdk = require("algosdk");
+const algodClient = new algosdk.Algodv2(process.env.ALGOD_TOKEN, process.env.ALGOD_SERVER, process.env.ALGOD_PORT);
+const creator = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_CREATOR);
+
+const submitToNetwork = async (signedTxn) => {
+
+    // Send transaction
+    const tx = await algodClient.sendRawTransaction(signedTxn).do();
+    console.log("Transaction: ", + tx.txId);
+
+    // Wait for transaction to be confirmed
+    confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4)
+
+    // Get the completed transaction
+    console.log("Transaction" + tx.txId + "confirmed in round" + confirmedTxn["confirmed-round"])
+
+    return confirmedTxn
+}
+
+const createNFT = async () => {
+
+    const from = creator.addr;
+    const defaultFrozen = false;
+    const unitName = "AFNFT";
+    const assetName = "Algo Foundry NFT";
+    const assetURL = "https://path/to/my/nft/asset/metadata.json";
+    const manager = creator.addr;
+    const reserve = undefined;
+    const freeze = undefined;
+    const clawback = undefined;
+    const total = 1;
+    const decimals = 0;
+
+    const suggestedParams = await algodClient.getTransactionParams().do();
+
+    // Create the asset creation transaction
+
+    const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+        from,
+        defaultFrozen,
+        unitName,
+        assetName,
+        assetURL,
+        manager,
+        reserve,
+        freeze,
+        clawback,
+        total,
+        decimals,
+        suggestedParams
+    });
+
+    const signedTxn = txn.signTxn(creator.sk);
+    const confirmedTxn = await submitToNetwork(signedTxn);
+
+    return confirmedTxn["asset-index"];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+(
+    async () => {
+
+    }
+)();
